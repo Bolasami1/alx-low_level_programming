b447aa10d3e249733daa210a4bd930073a45cfe1
root <git config --global user.email>
2023-01-03 05:54:13 -0800

    task6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,5 +1,12 @@
-void print_line(int n)
+#include "main.h"
 
+/**
+ * print_line - check for a digit
+ * @n : number of _ to be printed
+ * Return: void
+ */
+
+void print_line(int n)
 {
 	int i = 0;
 
